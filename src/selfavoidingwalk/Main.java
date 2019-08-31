b1fc6c3fdc5aa5fcd93d5fc218836182ccf4d2bf
Adnan Rahin <adnan.rahin13@gmail.com>
2019-08-31 00:18:26 -0400

    Des

--- src/selfavoidingwalk/Main.java
+++ src/selfavoidingwalk/Main.java
@@ -2,8 +2,10 @@ package selfavoidingwalk;
 
 public class Main {
 
-	public static void main(String args[]) {
+	public static int directions[][] = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+
+	public static void main(String[] args) {
 
 	}
-	
+
 }
