bd4ea4747535b7f72196744665e55bb384110858
Arx1971 <adnan.rahin13@gmail.com>
2019-09-06 21:09:11 -0400

    Data Ploted

--- src/selfavoidingwalk/Main.java
+++ src/selfavoidingwalk/Main.java
@@ -10,7 +10,8 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
 
-	final static int N_SAW = 1000000;
+	static Random random = new Random();
+	final static int N_SAW = random.nextInt(1000000);
 	static int directions2D[][] = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
 	static int directions3D[][] = { { 0, -1, 1 }, { 0, 1, -1 }, { -1, 0, 1 }, { -1, 1, 0 }, { 1, -1, 0 },
 			{ 1, 0, -1 } };
@@ -18,21 +19,98 @@ public class Main {
 			{ -1, 0, 0, 1 }, { -1, 1, 0, 0 }, { 0, -1, 0, 1 }, { 0, 0, -1, 1 }, { 0, 1, -1, 0 }, { 0, 1, 0, -1 },
 			{ 0, -1, 1, 0 } };
 
-	static Random random = new Random();
-
 	public static void main(String[] args) {
 
-		TwoDimensions twoD_ = new TwoDimensions();
+		/*
+		 * TwoDimensions twoD_ = new TwoDimensions();
+		 * 
+		 * Thread t1 = new Thread(twoD_);
+		 * 
+		 * t1.start();
+		 * 
+		 * ThreeDimensions threeD_ = new ThreeDimensions();
+		 * 
+		 * Thread t2 = new Thread(threeD_);
+		 * 
+		 * t2.start();
+		 */
 
-		Thread t1 = new Thread(twoD_);
+		FourDimensions fourd_ = new FourDimensions();
 
-		t1.start();
+		Thread t3 = new Thread(fourd_);
 
-		ThreeDimensions threeD_ = new ThreeDimensions();
+		t3.start();
 
-		Thread t2 = new Thread(threeD_);
+	}
 
-		t2.start();
+	public static class TwoDimensions extends Thread {
+
+		int steps;
+
+		public TwoDimensions() {
+			// Default Constructor
+		}
+
+		public TwoDimensions(int steps) {
+			this.steps = steps;
+		}
+
+		public static double slefAvoidingRandomWalk2D(int steps) {
+
+			List<point> pointsList = new ArrayList<point>();
+
+			Hashtable<Integer, List<point>> pointTable2d = new Hashtable<Integer, List<point>>();
+
+			for (int i = 0; i < N_SAW; i++) {
+
+				pointTable2d.put(i, new ArrayList<point>());
+
+				Set<String> visited = new HashSet<String>();
+
+				int x = 0;
+				int y = 0;
+
+				visited.add(Integer.toString(x) + "," + Integer.toString(y));
+
+				for (int j = 1; j <= steps; j++) {
+
+					int r = ThreadLocalRandom.current().nextInt(4);
+
+					x = x + directions2D[r][0];
+					y = y + directions2D[r][1];
+
+					String coordinate = Integer.toString(x) + "," + Integer.toString(y);
+
+					if (!visited.add(coordinate))
+						break;
+
+					if (visited.size() == steps)
+						pointsList.add(new point(x, y));
+
+					pointTable2d.get(i).add(new point(x, y)); // keep track of all the path with n steps
+
+				}
+			}
+
+			double squaredDistance = 0.0;
+
+			for (int i = 0; i < pointsList.size(); i++) {
+				squaredDistance += Math.pow(pointsList.get(i).x, 2) + Math.pow(pointsList.get(i).y, 2);
+			}
+
+			double avg = (double) (squaredDistance / N_SAW);
+
+			return avg;
+
+		}
+
+		public void run() {
+
+			for (int i = 1; i <= 40; i++) {
+				System.out.println(slefAvoidingRandomWalk2D(i) + " TWO");
+			}
+
+		}
 
 	}
 
@@ -48,7 +126,7 @@ public class Main {
 			// Default Constructor
 		}
 
-		public static double slefAvoidingRandomWlak3D(int steps) {
+		public static double slefAvoidingRandomWalk3D(int steps) {
 
 			List<tuple> tupleList = new ArrayList<tuple>();
 
@@ -103,65 +181,72 @@ public class Main {
 
 		public void run() {
 			for (int i = 1; i <= 40; i++) {
-				System.out.println(slefAvoidingRandomWlak3D(i) + "THREE");
+				System.out.println(slefAvoidingRandomWalk3D(i) + "THREE");
 			}
 		}
 
 	}
 
-	public static class TwoDimensions extends Thread {
+	public static class FourDimensions extends Thread {
 
 		int steps;
 
-		public TwoDimensions() {
-			// Default Constructor
-		}
-
-		public TwoDimensions(int steps) {
+		public FourDimensions(int steps) {
 			this.steps = steps;
 		}
 
-		public static double slefAvoidingRandomWlak2D(int steps) {
+		public FourDimensions() {
 
-			List<point> pointsList = new ArrayList<point>();
+		}
 
-			Hashtable<Integer, List<point>> pointTable2d = new Hashtable<Integer, List<point>>();
+		public static double slefAvoidingRandomWalk4D(int steps) {
+
+			List<fourDCordinates> fouDCordinatesList = new ArrayList<fourDCordinates>();
+
+			Hashtable<Integer, List<fourDCordinates>> fourDcordinatestable = new Hashtable<Integer, List<fourDCordinates>>();
 
 			for (int i = 0; i < N_SAW; i++) {
 
-				pointTable2d.put(i, new ArrayList<point>());
+				int x = 0;
+				int y = 0;
+				int z = 0;
+				int w = 0;
 
 				Set<String> visited = new HashSet<String>();
 
-				int x = 0;
-				int y = 0;
+				visited.add(Integer.toString(x) + "," + Integer.toString(y) + "," + Integer.toString(z) + ","
+						+ Integer.toString(w));
 
-				visited.add(Integer.toString(x) + "," + Integer.toString(y));
+				fourDcordinatestable.put(i, new ArrayList<fourDCordinates>());
 
 				for (int j = 1; j <= steps; j++) {
 
-					int r = ThreadLocalRandom.current().nextInt(4);
+					int r = ThreadLocalRandom.current().nextInt(12);
 
-					x = x + directions2D[r][0];
-					y = y + directions2D[r][1];
-
-					String coordinate = Integer.toString(x) + "," + Integer.toString(y);
+					x = x + direction4D[r][0];
+					y = y + direction4D[r][1];
+					z = z + direction4D[r][2];
+					w = w + direction4D[r][3];
+					String coordinate = Integer.toString(x) + "," + Integer.toString(y) + "," + Integer.toString(z)
+							+ "," + Integer.toString(w);
 
 					if (!visited.add(coordinate))
 						break;
 
 					if (visited.size() == steps)
-						pointsList.add(new point(x, y));
+						fouDCordinatesList.add(new fourDCordinates(x, y, z, w));
 
-					pointTable2d.get(i).add(new point(x, y)); // keep track of all the path with n steps
+					fourDcordinatestable.get(i).add(new fourDCordinates(x, y, z, w));
 
 				}
+
 			}
 
 			double squaredDistance = 0.0;
 
-			for (int i = 0; i < pointsList.size(); i++) {
-				squaredDistance += Math.pow(pointsList.get(i).x, 2) + Math.pow(pointsList.get(i).y, 2);
+			for (int i = 0; i < fouDCordinatesList.size(); i++) {
+				squaredDistance += Math.pow(fouDCordinatesList.get(i).x, 2) + Math.pow(fouDCordinatesList.get(i).y, 2)
+						+ Math.pow(fouDCordinatesList.get(i).z, 2) + Math.pow(fouDCordinatesList.get(i).w, 2);
 			}
 
 			double avg = (double) (squaredDistance / N_SAW);
@@ -173,7 +258,7 @@ public class Main {
 		public void run() {
 
 			for (int i = 1; i <= 40; i++) {
-				System.out.println(slefAvoidingRandomWlak2D(i) + " TWO");
+				System.out.println(slefAvoidingRandomWalk4D(i));
 			}
 
 		}
@@ -215,6 +300,33 @@ public class Main {
 		public String toString() {
 			return Integer.toString(x) + "," + Integer.toString(y) + "," + Integer.toString(z);
 		}
+	}
+
+	public static class fourDCordinates {
+
+		int x;
+		int y;
+		int z;
+		int w;
+
+		public fourDCordinates(int x, int y, int z, int w) {
+
+			this.x = x;
+			this.y = y;
+			this.z = z;
+			this.w = w;
+
+		}
+
+		public fourDCordinates() {
+			// TODO Auto-generated constructor stub
+		}
+
+		public String toString() {
+			return Integer.toString(x) + "," + Integer.toString(y) + "," + Integer.toString(z) + ","
+					+ Integer.toString(w);
+		}
+
 	}
 
 }
