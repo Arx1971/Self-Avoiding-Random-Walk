f73fe4dbc7574d21cad3808cf913b27be9bde5cf
arx1971 <adnan.rahin13@gmail.com>
2019-09-11 23:26:28 -0400

    UPdate

--- src/selfavoidingwalk/Main.java
+++ src/selfavoidingwalk/Main.java
@@ -11,7 +11,7 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Main {
 
 	static Random random = new Random();
-	final static int SAMPLES = random.nextInt(5000000);
+	final static int SAMPLES = random.nextInt(500000);
 	static int directions2D[][] = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
 	static int directions3D[][] = { { 1, 0, 0 }, { -1, 0, 0 }, { 0, 1, 0 }, { 0, -1, 0 }, { 0, 0, 1 }, { 0, 0, -1 } };
 	static int directions4D[][] = { { 0, 0, 0, -1 }, { 0, 0, -1, 0 }, { 0, -1, 0, 0 }, { -1, 0, 0, 0 }, { 0, 0, 0, 1 },
