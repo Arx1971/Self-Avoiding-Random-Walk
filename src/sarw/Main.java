40daad0d3d8071a532f8b491fd89da6f4c526c41
adnanrahin <adnan.rahin13@gmail.com>
2019-09-12 20:19:15 -0400

    Update

--- src/sarw/Main.java
+++ src/sarw/Main.java
@@ -1,6 +1,8 @@
 package sarw;
 
+import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
@@ -8,8 +10,8 @@ import java.util.Set;
 
 public class Main {
 
-	static int directios[][] = { { 0, 1 }, { 0, -1 }, { -1, 0 }, { 1, 0 } };
-	static int NumberOfTrials = 5000000;
+	static int directions[][] = { { 0, 1 }, { 0, -1 }, { -1, 0 }, { 1, 0 } };
+	static int N_SAW = 5000000;
 	static Random random = new Random();
 
 	public static void main(String args[]) {
@@ -19,6 +21,17 @@ public class Main {
 		Thread thread = new Thread(twoD);
 		thread.start();
 
+		System.out.println("<\t" + "R^2" + "\t>\t\t" + "f_Saw(n)");
+
+		/*
+		 * TwoDimensionWalk twoD[] = new TwoDimensionWalk[21]; Thread thread[] = new
+		 * Thread[21]; for (int i = 0; i <= 20; i++) { twoD[i] = new TwoDimensionWalk(i
+		 * + 10); thread[i] = new Thread(twoD[i]); thread[i].start(); }
+		 * 
+		 * for (int i = 0; i <= 20; i++) { try { thread[i].join(); } catch (Exception e)
+		 * { // TODO: handle exception } }
+		 */
+
 	}
 
 	public static class TwoDimensionWalk extends Thread {
@@ -26,18 +39,20 @@ public class Main {
 		int steps;
 
 		public TwoDimensionWalk() {
-			// TODO Auto-generated constructor stub
+
 		}
 
 		public TwoDimensionWalk(int steps) {
 			this.steps = steps;
 		}
 
+		static int N_SAW_TOTAL = 0;
+
 		public static double selfAvoidingWalk(int steps) {
 
-			List<Double> end_to_end_Distance = new ArrayList<Double>();
-
-			for (int i = 1; i <= NumberOfTrials; i++) {
+			List<Double> end_to_end_Distance = Collections.synchronizedList(new ArrayList<Double>());
+			N_SAW_TOTAL = 0;
+			for (int i = 1; i <= N_SAW; i++) {
 
 				Set<String> visited = new HashSet<String>();
 				List<String> path = new ArrayList<String>();
@@ -50,8 +65,8 @@ public class Main {
 
 					int r = random.nextInt(4);
 
-					x += directios[r][0];
-					y += directios[r][1];
+					x += directions[r][0];
+					y += directions[r][1];
 
 					path.add(Integer.toString(x) + "," + Integer.toString(y));
 
@@ -61,15 +76,12 @@ public class Main {
 					if (visited.size() == steps) {
 						double distance = Math.pow(x, 2) + Math.pow(y, 2);
 						end_to_end_Distance.add(distance);
-						// System.out.println("Cordinates: " + path + "Size: " + path.size());
-						// System.out.println("End To End Distance: " + distance);
+						// System.out.println(path + " " + " " + path.size() + " " + distance);
 					}
 
 				}
 
 			}
-
-			// System.out.println("LIST OF END TO END DISTANCE: " + end_to_end_Distance);
 
 			double mean = 0.0;
 
@@ -79,14 +91,33 @@ public class Main {
 
 			double avg = (double) mean / end_to_end_Distance.size();
 
+			N_SAW_TOTAL = end_to_end_Distance.size();
+
 			return avg;
 
 		}
 
 		public void run() {
 
-			for (int i = 10; i <= 30; i++) {
-				System.out.println(selfAvoidingWalk(i));
+			// System.out.println(steps + " -> " + selfAvoidingWalk(steps));
+
+			double solutions = 0.0;
+			double fsaw = 0.0;
+			String twoDfliepath = "/home/adnanrahin/eclipse-workspace/Self-Avoiding-Random-Walk/result.txt";
+			try (FileWriter writer = new FileWriter(twoDfliepath)) {
+				for (int i = 10; i <= 40; i++) {
+					solutions = selfAvoidingWalk(i);
+					fsaw = ((double) N_SAW_TOTAL / N_SAW * Thread.activeCount());
+					System.out.println(i + " " + solutions + "\t\t"
+							+ (double) ((double) N_SAW_TOTAL / N_SAW * Thread.activeCount()));
+
+					writer.write(Double.toString(solutions) + "\t\t" + Double.toString(fsaw));
+					writer.write("\n");
+				}
+				writer.close();
+
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 
 		}
